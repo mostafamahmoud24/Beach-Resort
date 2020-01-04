0b5d8ebf85cd794d2a33d75592406d19edb75b8f
Mostafa Mahmoud <mostafamahmoud24@gmail.com>
2020-01-04 22:28:42 +0200

    new commit

--- src/context.jsx
+++ src/context.jsx
@@ -1,12 +1,40 @@
 import React, { Component } from "react";
+import items from "./data";
 
 const RoomContext = React.createContext();
 
 class RoomProvider extends Component {
   state = {
-    greeting: "hello",
-    name: "Mostafa"
+    rooms: [],
+    sortedRooms: [],
+    featuredRooms: [],
+    loading: true
   };
+
+  //getting the formated Data
+
+  componentDidMount() {
+    let rooms = this.formatData(items);
+    let featuredRooms = rooms.filter(room => room.featured === true);
+    this.setState({
+      rooms: rooms,
+      featuredRooms: featuredRooms,
+      sortedRooms: rooms,
+      loading: false
+    });
+  }
+
+  //formatting the Data
+
+  formatData(items) {
+    let tempItems = items.map(item => {
+      let id = item.sys.id;
+      let images = item.fields.images.map(image => image.fields.file.url);
+      let room = { ...item.fields, images, id };
+      return room;
+    });
+    return tempItems;
+  }
 
   render() {
     return (
