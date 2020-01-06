0d590bd72ac38a059e4bd958a3df081a2b7d644d
Mostafa Mahmoud <mostafamahmoud24@gmail.com>
2020-01-06 03:02:38 +0200

    new commit

--- src/components/RoomsContainer.jsx
+++ src/components/RoomsContainer.jsx
@@ -1,14 +1,26 @@
 import React from "react";
 import RoomsFilter from "./RoomsFilter";
 import RoomsList from "./RoomsList";
+import { RoomConsumer } from "../context";
+import Loading from "./Loading";
 
 function RoomsContainer() {
   return (
-    <div>
-      Hello from Rooms Container
-      <RoomsFilter />
-      <RoomsList />
-    </div>
+    <RoomConsumer>
+      {value => {
+        const { loading, sortedRooms, rooms } = value;
+        if (loading) {
+          return <Loading />;
+        }
+        return (
+          <div>
+            Hello from Rooms Container
+            <RoomsFilter rooms={rooms} />
+            <RoomsList rooms={sortedRooms} />
+          </div>
+        );
+      }}
+    </RoomConsumer>
   );
 }
 
